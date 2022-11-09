042a71d1f2da1d92b264e45255c6f5d52447cd78
Md Rezone Ahamed <rezoneprinceme@gmail.com>
2022-11-09 12:03:00 +0600

    basic setup ready

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Cooking Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running http://localhost:${port}`);
+});
