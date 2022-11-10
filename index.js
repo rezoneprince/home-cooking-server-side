d9dd4f3a8dd5cd266e72e4667b3d09b5383d666d
Md Rezone Ahamed <rezoneprinceme@gmail.com>
2022-11-10 15:48:31 +0600

    Product api ready

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ovxjb0p.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const productsCollection = client.db("homeCooking").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/product3", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
+
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Home Cooking Server");
