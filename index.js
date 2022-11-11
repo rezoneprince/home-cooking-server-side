4960cd31fd51c8c2e1d9f4ae3d761a7c8fc02a57
Md Rezone Ahamed <rezoneprinceme@gmail.com>
2022-11-11 22:11:44 +0600

    add review api

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     const productsCollection = client.db("homeCooking").collection("products");
+    const reviewsCollection = client.db("homeCooking").collection("reviews");
 
     app.get("/products", async (req, res) => {
       const query = {};
@@ -38,13 +39,77 @@ const run = async () => {
     app.get("/product3", async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
-      const services = await cursor.limit(3).toArray();
-      res.send(services);
+      const result = await cursor.limit(3).toArray();
+      res.send(result);
+    });
+
+    app.get("/my-products", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     app.post("/product", async (req, res) => {
       const order = req.body;
       const result = await productsCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.patch("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await productsCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/my-review", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/review5", async (req, res) => {
+      let query = {};
+      const cursor = reviewsCollection.find(query);
+      const result = await cursor.limit(5).toArray();
+      res.send(result);
+    });
+
+    app.post("/review", async (req, res) => {
+      const order = req.body;
+      const result = await reviewsCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
